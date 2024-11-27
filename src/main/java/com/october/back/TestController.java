fce388e3e681b60c8b38c299a6ee3d77e24f2a52
jwj <dnwls16071@naver.com>
2024-11-28 00:19:03 +0900

    [test] 무중단 배포 테스트용 컨트롤러 추가

---
+++ src/main/java/com/october/back/TestController.java
@@ -0,0 +1,15 @@
+package com.october.back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "무중단 배포 테스트 컨트롤러";
+	}
+}
