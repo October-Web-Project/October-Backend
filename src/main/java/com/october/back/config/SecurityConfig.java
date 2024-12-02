7d0083d58f7133ed4e88a567efddd5b7aedb5542
soapcookie <sgua0720@gmail.com>
2024-12-02 14:25:40 +0900

    [feat] : redis,securityConfig 작성

---
+++ src/main/java/com/october/back/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.october.back.config;
+
+import com.october.back.jwt.JwtAuthTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthTokenFilter jwtAuthTokenFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 비활성화
+                .formLogin(formLogin -> formLogin.disable()) // 폼 로그인 비활성화
+                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()))  // CORS 기본 설정
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin()) // 동일 출처에서만 iframe 허용
+                        .httpStrictTransportSecurity(hsts -> hsts
+                                .includeSubDomains(true)
+                                .maxAgeInSeconds(31536000) // HSTS 설정
+                        )
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/") // 로그아웃 성공 시 "/"로 리다이렉트
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error/**", "/auth/**", "/home/**", "/review/**").permitAll() // 특정 경로 허용
+                        .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN") // 관리자 권한 필요
+                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                )
+                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터 추가
+                .build();
+    }
+}
+
