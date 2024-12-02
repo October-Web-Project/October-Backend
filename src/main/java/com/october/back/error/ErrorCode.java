086f89e7139ef91d03635044b1524f447603e4c5
soapcookie <sgua0720@gmail.com>
2024-12-02 16:40:24 +0900

    [fix] : 카카오, 네이버 에러코드 추가

--- src/main/java/com/october/back/error/ErrorCode.java
+++ src/main/java/com/october/back/error/ErrorCode.java
@@ -15,9 +15,13 @@ public enum ErrorCode {
     FORBIDDEN_EXCEPTION(HttpStatus.FORBIDDEN, "403", "You do not have permission"),
     NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "404", "Not found matched data"),
     DUPLICATE_EXCEPTION(HttpStatus.BAD_REQUEST, "400", "cannot Allowed Duplicated "),
-    Internal_Error(HttpStatus.INTERNAL_SERVER_ERROR, "500","예상하치 못한 에러, 긴급")
-
-    ;
+    Internal_Error(HttpStatus.INTERNAL_SERVER_ERROR, "500","예상하치 못한 에러, 긴급"),
+    //카카오 관련 오류코드 추가
+    KAKAO_ACCESS_TOKEN_FAILED(HttpStatus.UNAUTHORIZED, "401", "Failed to get access token from Kakao"),
+    KAKAO_USER_INFO_FAILED(HttpStatus.UNAUTHORIZED, "401", "Failed to get user info from Kakao"),
+    // 네이버 관련 오류 코드 추가
+    NAVER_ACCESS_TOKEN_FAILED(HttpStatus.UNAUTHORIZED, "401", "Failed to get access token from Naver"),
+    NAVER_USER_INFO_FAILED(HttpStatus.UNAUTHORIZED, "401", "Failed to get user info from Naver");
 
 
     private final HttpStatus status;
