8ffb67b07b44a311e9ebcf2537e588bf2ac36ad1
jwj <dnwls16071@naver.com>
2024-12-03 20:09:45 +0900

    [feat] application.yml 변경에 따른 AWS S3 설정 재수정

--- src/main/java/com/october/back/config/AwsS3Config.java
+++ src/main/java/com/october/back/config/AwsS3Config.java
@@ -1,5 +1,6 @@
 package com.october.back.config;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
@@ -11,22 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsS3Config {
 
-	@Value("${aws.access-key}")
+	@Value("${cloud.aws.credentials.access-key}")
 	private String accessKey;
 
-	@Value("${aws.secret-key}")
+	@Value("${cloud.aws.credentials.secret-key}")
 	private String secretKey;
 
-	@Value("${aws.region}")
+	@Value("${cloud.aws.region}")
 	private String region;
 
 	@Bean
 	public AmazonS3 amazonS3Client() {
-		BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-		return AmazonS3ClientBuilder.standard()
-				.withRegion(region)
+		return AmazonS3ClientBuilder
+				.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(region)
 				.build();
 	}
 }
