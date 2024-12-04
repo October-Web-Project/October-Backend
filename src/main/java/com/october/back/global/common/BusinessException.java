e90b35ce06bc61ae8fcfc1f3b507f1c422f3c851
jwj <dnwls16071@naver.com>
2024-12-05 02:12:43 +0900

    [fix] API 예외 스펙 변경에 따른 커스텀 예외 수정

--- src/main/java/com/october/back/global/common/BusinessException.java
+++ src/main/java/com/october/back/global/common/BusinessException.java
@@ -2,27 +2,17 @@ package com.october.back.global.common;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 public abstract class BusinessException extends RuntimeException {
 
-  private final ErrorCode errorCode;
-  private List<ErrorResponse.FieldError> errors = new ArrayList<>();
+    private final ErrorCode errorCode;
 
-  protected BusinessException(String message, ErrorCode errorCode) {
-    super(message);
-    this.errorCode = errorCode;
-  }
+    protected BusinessException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
 
-  protected BusinessException(ErrorCode errorCode) {
-    this.errorCode = errorCode;
-  }
-
-  protected BusinessException(ErrorCode errorCode, List<ErrorResponse.FieldError> errors) {
-    super(errorCode.getMessage());
-    this.errorCode = errorCode;
-    this.errors = errors;
-  }
+    protected BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
--- src/main/java/com/october/back/global/exception/ReviewException.java
+++ src/main/java/com/october/back/global/exception/ReviewException.java
@@ -1,6 +1,7 @@
 package com.october.back.global.exception;
 
 public class ReviewException extends RuntimeException {
+
 	public ReviewException(String message) {
 		super(message);
 	}
