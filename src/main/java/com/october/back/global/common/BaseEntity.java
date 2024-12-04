f0cb94b76b9cbf47899f629a6b265508ab0e000d
jwj <dnwls16071@naver.com>
2024-12-05 00:00:22 +0900

    [feat] JPA 엔티티 자동 감사(audit) 기능 추가

--- src/main/java/com/october/back/global/common/BaseEntity.java
+++ src/main/java/com/october/back/global/common/BaseEntity.java
@@ -5,17 +5,18 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
 @Getter
+@EntityListeners(AuditingEntityListener.class)
+@EnableJpaAuditing
 @MappedSuperclass
 public abstract class BaseEntity {
 
