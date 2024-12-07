7b31bfb996dbcc81302de7c9efbdd6786197851c
soapcookie <sgua0720@gmail.com>
2024-12-07 16:29:10 +0900

    [fix] : 매핑 주석처리, email 필드 추가

--- src/main/java/com/october/back/entity/Users.java
+++ src/main/java/com/october/back/entity/Users.java
@@ -1,12 +1,17 @@
 package com.october.back.entity;
 
+import com.october.back.error.ErrorCode;
+import com.october.back.error.exception.ClientException;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users extends BaseEntity {
 
     @Column(nullable = false)
@@ -15,10 +20,20 @@ public class Users extends BaseEntity {
     @Column(unique = true, nullable = false)
     private String nickName; // 사용자 닉네임(중복 불가, 고유 식별 가능)
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Review> reviewList; //사용자가 쓴 리뷰 목록
+    @Column(unique = true, nullable = false)
+    private String email; // OAuth 로그인 시 가져올 사용자 이메일(중복 불가, 고유 식별 가능)
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    private List<Comment> commentList; //사용자가 쓴 댓글 목록
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+//    private List<Review> reviewList; //사용자가 쓴 리뷰 목록
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+//    private List<Comment> commentList; //사용자가 쓴 댓글 목
+
+    public void changeNickname(String newNickname) {
+        if (newNickname == null || newNickname.trim().isEmpty()) {
+            throw new ClientException(ErrorCode.PARAMETER_VALID_EXCEPTION);
+        }
+        this.nickName = newNickname;
+    }
 
 }
