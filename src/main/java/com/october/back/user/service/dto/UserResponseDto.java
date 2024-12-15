574e2030bb81398cf3017c31550ec80afc81358a
JShistory <behappy0916@naver.com>
2024-12-16 01:03:30 +0900

    [feat] 유저 Entity에 맞게 responseDto 수정

--- src/main/java/com/october/back/user/service/dto/UserResponseDto.java
+++ src/main/java/com/october/back/user/service/dto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package com.october.back.user.service.dto;
 
+import com.october.back.user.entity.UserRole;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,11 +11,17 @@ public class UserResponseDto {
     private Long id;
     private String name;
     private String email;
+    private String nickname;
+    private UserRole userRole;
+    private Boolean isActive;
 
     @Builder
-    public UserResponseDto(Long id, String name, String email) {
+    public UserResponseDto(Long id, String name, String email, String nickname, UserRole userRole, boolean isActive) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.nickname = nickname;
+        this.userRole = userRole;
+        this.isActive = isActive;
     }
 }
