acae52296e24d637ad53bfce33fabf08b9a13306
JShistory <behappy0916@naver.com>
2024-12-16 01:42:09 +0900

    [feat] 유저 서비스 추가

--- src/main/java/com/october/back/user/service/UserService.java
+++ src/main/java/com/october/back/user/service/UserService.java
@@ -1,17 +1,14 @@
 package com.october.back.user.service;
 
-import com.october.back.global.exception.UserException;
 import com.october.back.user.entity.UserRole;
 import com.october.back.user.entity.Users;
 import com.october.back.user.service.dto.UserRequestDto;
 import com.october.back.user.service.dto.UserResponseDto;
 import com.october.back.user.repository.UserRepository;
-import com.october.back.global.common.ErrorCode;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -28,4 +25,19 @@ public class UserService {
         Users savedUsers = userRepository.save(users);
         return savedUsers.getId();
     }
+
+    public UserResponseDto getUser(Long id) {
+        Optional<Users> users = userRepository.findById(id);
+
+        return users.get().convertResponseDto();
+    }
+
+    public Optional<Users> findByName(String name) {
+        return userRepository.findByName(name);
+    }
+    @Transactional
+    public void updateUser(Users user) {
+        userRepository.save(user);
+    }
+
 }
