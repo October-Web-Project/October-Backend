bba9692d54722d0f6617173cef62c102cf18ca93
JShistory <behappy0916@naver.com>
2024-12-16 16:24:17 +0900

    [feat] 유저 생성할 때 예외처리 추가

--- src/main/java/com/october/back/user/service/UserService.java
+++ src/main/java/com/october/back/user/service/UserService.java
@@ -1,14 +1,18 @@
 package com.october.back.user.service;
 
+import com.october.back.global.exception.UserException;
 import com.october.back.user.entity.UserRole;
 import com.october.back.user.entity.Users;
 import com.october.back.user.service.dto.UserRequestDto;
-import com.october.back.user.service.dto.UserResponseDto;
 import com.october.back.user.repository.UserRepository;
+
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.october.back.global.common.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Service
@@ -17,24 +21,26 @@ public class UserService {
 
     @Transactional
     public Long createUser(UserRequestDto userRequestDto) {
-        Users users = new Users();
-        users.setName(userRequestDto.getName());
-        users.setEmail(userRequestDto.getEmail());
-        users.setNickname(userRequestDto.getNickname());
-        users.setUserRole(UserRole.USER);
-        Users savedUsers = userRepository.save(users);
+        Optional<Users> existingUser = userRepository.findByName(userRequestDto.getName());
+
+        if (existingUser.isPresent()) {
+            throw new UserException(ALREADY_EXIST_USER);
+        }
+
+        Users newUser = Users.builder()
+                .name(userRequestDto.getName())
+                .email(userRequestDto.getEmail())
+                .nickname(userRequestDto.getNickname())
+                .userRole(UserRole.USER)
+                .build();
+        Users savedUsers = userRepository.save(newUser);
         return savedUsers.getId();
-    }
-
-    public UserResponseDto getUser(Long id) {
-        Optional<Users> users = userRepository.findById(id);
-
-        return users.get().convertResponseDto();
     }
 
     public Optional<Users> findByName(String name) {
         return userRepository.findByName(name);
     }
+
     @Transactional
     public void updateUser(Users user) {
         userRepository.save(user);
