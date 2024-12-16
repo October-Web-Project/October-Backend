1c6592008feddfe3d7c712137bbcc5872fc2d564
JShistory <behappy0916@naver.com>
2024-12-16 16:37:18 +0900

    [feat] NoArgsConstructor 어노테이션 추가

--- src/main/java/com/october/back/user/entity/Users.java
+++ src/main/java/com/october/back/user/entity/Users.java
@@ -6,12 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Users extends BaseEntity {
     @Column(name = "name", nullable = false)
