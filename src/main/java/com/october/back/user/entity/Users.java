590fb057157e3d514e34c4ca9536a91a14f66c43
JShistory <behappy0916@naver.com>
2024-12-16 16:24:01 +0900

    [feat] user 빌더 추가

--- src/main/java/com/october/back/user/entity/Users.java
+++ src/main/java/com/october/back/user/entity/Users.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,6 +28,14 @@ public class Users extends BaseEntity {
 
     @Column(name = "is_active", nullable = false)
     private Boolean isActive = true; // 기본값: 활성 상태
+
+    @Builder
+    public Users(String name, String nickname, String email, UserRole userRole) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.userRole = userRole;
+    }
 
     public UserResponseDto convertResponseDto() {
         return UserResponseDto.builder()
