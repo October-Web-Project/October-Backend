c9681bffee116b99da1dbfd081b065ced7824f28
JShistory <behappy0916@naver.com>
2025-01-03 15:37:44 +0900

    [feat] (임시) 프론트 요청으로 redirect url 수정

--- src/main/java/com/october/back/security/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/october/back/security/oauth2/CustomSuccessHandler.java
@@ -39,8 +39,20 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         String token = jwtUtil.createJwt(username, role, 60*60*1000L);
         response.addCookie(createCookie("Authorization", token));
-        // todo : 프론트 주소 맞춰서 바꿔야 함.
-        response.sendRedirect("/api/main");
+        // OAuth2 제공자 확인
+        String registrationId = customUserDetails.getUserDto().getName().split(" ")[0];
+
+        // 동적으로 리다이렉트 URL 설정
+        // todo: 추후 프론트 배포 주소에 맞게 수정해야됨
+        String redirectUrl;
+        if ("kakao".equalsIgnoreCase(registrationId)) {
+            redirectUrl = "http://localhost:3000/api/auth/callback/kakao";
+        } else if ("naver".equalsIgnoreCase(registrationId)) {
+            redirectUrl = "http://localhost:3000/api/auth/callback/naver";
+        } else {
+            redirectUrl = "/api/main"; // 기본 리다이렉트 URL
+        }
+        response.sendRedirect(redirectUrl);
     }
 
     private Cookie createCookie(String key, String value) {
